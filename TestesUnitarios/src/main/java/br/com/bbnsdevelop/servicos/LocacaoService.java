e37bf38326a52923ea674a024b41229f4faab7b3
bbnsdevelop <bbnsdevelop@gmail.com>
2020-02-05 17:37:54 -0300

    retirando metodo de teste da classe de serviço

--- TestesUnitarios/src/main/java/br/com/bbnsdevelop/servicos/LocacaoService.java
+++ TestesUnitarios/src/main/java/br/com/bbnsdevelop/servicos/LocacaoService.java
@@ -1,7 +1,6 @@
 package br.com.bbnsdevelop.servicos;
 
 import static br.com.bbnsdevelop.utils.DataUtils.adicionarDias;
-import static br.com.bbnsdevelop.utils.DataUtils.formatDateToDDMMYYY;
 
 import java.util.Date;
 
@@ -29,18 +28,5 @@ public class LocacaoService {
 		return locacao;
 	}
 
-	public static void main(String[] args) {
-		LocacaoService locacaoService = new LocacaoService();
-		
-		Usuario usuario = new Usuario("Bruno"); 
-		Filme filme = new Filme("A fulga das galinhas", 1, 5.7);
-		
-		Locacao response = locacaoService.alugarFilme(usuario,filme);
-		System.out.println(response.getValor());
-		System.out.println(formatDateToDDMMYYY(response.getDataLocacao()));
-		System.out.println(formatDateToDDMMYYY(response.getDataRetorno()));
-		System.out.println(response.getUsuario().getNome());
-		
-		
-	}
+	
 }
