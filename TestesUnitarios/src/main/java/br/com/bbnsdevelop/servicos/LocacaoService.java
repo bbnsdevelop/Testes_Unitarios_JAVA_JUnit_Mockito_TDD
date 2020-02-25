4d854013f6d7d3a3f8c472b62fb530b11920fc31
bbnsdevelop <bbnsdevelop@gmail.com>
2020-02-24 23:31:37 -0300

    desconto na locação acima de 3 filmes

--- TestesUnitarios/src/main/java/br/com/bbnsdevelop/servicos/LocacaoService.java
+++ TestesUnitarios/src/main/java/br/com/bbnsdevelop/servicos/LocacaoService.java
@@ -13,49 +13,58 @@ import br.com.bbnsdevelop.exceptions.FilmeSemEstoqueException;
 import br.com.bbnsdevelop.exceptions.LocadoraException;
 
 public class LocacaoService {
-	
+
 	public Locacao alugarFilme(Usuario usuario, List<Filme> filmes) throws LocadoraException, FilmeSemEstoqueException {
-		
-		if(usuario == null) {
+
+		if (usuario == null) {
 			throw new LocadoraException("Usuário não pode ser vazio");
 		}
-		
-		if(filmes == null || filmes.isEmpty() || filmes.size() == 0) {
+
+		if (filmes == null || filmes.isEmpty() || filmes.size() == 0) {
 			throw new LocadoraException("Filme não pode ser vazio");
 		}
-		
-		if(filmes.isEmpty()) {
+
+		if (filmes.isEmpty()) {
 			throw new LocadoraException("Filme não podem ser vazios");
 		}
 		List<Filme> FilmeLocacoes = new ArrayList<>();
-		
-		filmes.forEach(filme -> {
+
+		for(int i = 0; i < filmes.size(); i ++){
 			try {
-				if(filme.getEstoque() == 0) {
+				if (filmes.get(i).getEstoque() == 0) {
 					throw new FilmeSemEstoqueException("filme sem estoque");
 				}
-				FilmeLocacoes.add(filme);
+				if (i == 2) {
+					filmes.get(i).setPrecoLocacao((filmes.get(i).getPrecoLocacao() - filmes.get(i).getPrecoLocacao() * 25 / 100));
+				} else if (i == 3) {
+					filmes.get(i).setPrecoLocacao((filmes.get(i).getPrecoLocacao() - filmes.get(i).getPrecoLocacao() * 50 / 100));
+				} else if (i == 4) {
+					filmes.get(i).setPrecoLocacao((filmes.get(i).getPrecoLocacao() - filmes.get(i).getPrecoLocacao() * 75 / 100));
+				} else if (i == 5) {
+					filmes.get(i).setPrecoLocacao((filmes.get(i).getPrecoLocacao() - filmes.get(i).getPrecoLocacao() * 100 / 100));
+				}
+
+				FilmeLocacoes.add(filmes.get(i));
 			} catch (FilmeSemEstoqueException e) {
 				throw new RuntimeException(e);
-				
+
 			}
-		});
-		Locacao locacao = new Locacao();			
+		}
+		
+		Locacao locacao = new Locacao();
 		locacao.setUsuario(usuario);
-		locacao.setDataLocacao(new Date());				
+		locacao.setDataLocacao(new Date());
 		Date dataEntrega = new Date();
-		//Entrega no dia seguinte
+		// Entrega no dia seguinte
 		dataEntrega = adicionarDias(dataEntrega, 1);
 		locacao.setDataRetorno(dataEntrega);
 		locacao.setFilme(FilmeLocacoes);
-		locacao.setValor(filmes.stream().mapToDouble(f -> f.getPrecoLocacao()).sum()); 
+		locacao.setValor(filmes.stream().mapToDouble(f -> f.getPrecoLocacao()).sum());
 
-		
-		//Salvando a locacao...	
-		//TODO adicionar método para salvar
+		// Salvando a locacao...
+		// TODO adicionar método para salvar
 		return locacao;
-		
+
 	}
 
-	
 }
--- TestesUnitarios/src/test/java/br/com/bbnsdevelop/servicos/LocacaoServiceTest.java
+++ TestesUnitarios/src/test/java/br/com/bbnsdevelop/servicos/LocacaoServiceTest.java
@@ -22,179 +22,243 @@ import org.junit.rules.ExpectedException;
 import br.com.bbnsdevelop.entidades.Filme;
 import br.com.bbnsdevelop.entidades.Locacao;
 import br.com.bbnsdevelop.entidades.Usuario;
-import br.com.bbnsdevelop.exceptions.FilmeSemEstoqueException;
 import br.com.bbnsdevelop.exceptions.LocadoraException;
 
 public class LocacaoServiceTest {
 
-	
 	private static LocacaoService locacaoService;
-	
+
 	@Rule
 	public ErrorCollector error = new ErrorCollector();
-	
+
 	@Rule
 	public ExpectedException ex = ExpectedException.none();
-	
+
 	/*
 	 * Execução antes de cada caso de teste
 	 * 
-	@Before
-	public void setup() {
-		this.locacaoService = new LocacaoService();
-	}
-	@After
-	public void tearDown() {
-		System.out.println("After");
-	}*/
-	
-	
-	/*Com a anotação instancia a classe antes dos medotos serem executados é preciso colocá-los estaticos de modo que o junit possa utilizá-lo
-	 * Do contário irá obter erro de inicialização
-	 * */
+	 * @Before public void setup() { this.locacaoService = new LocacaoService(); }
+	 * 
+	 * @After public void tearDown() { System.out.println("After"); }
+	 */
+
+	/*
+	 * Com a anotação instancia a classe antes dos medotos serem executados é
+	 * preciso colocá-los estaticos de modo que o junit possa utilizá-lo Do contário
+	 * irá obter erro de inicialização
+	 */
 	@BeforeClass
 	public static void setup() {
 		locacaoService = new LocacaoService();
 	}
+
 	@AfterClass
 	public static void tearDown() {
 		locacaoService = null;
 	}
-	
+
 	@Test
-	public void testeLocacao() throws Exception{
+	public void testeLocacao() throws Exception {
 		Locacao response = mockLocacao();
-		error.checkThat(response , not(equalTo(null)));
+		error.checkThat(response, not(equalTo(null)));
 		error.checkThat(response.getValor(), not(equalTo(0.0)));
 		error.checkThat(formatDateToDDMMYYY(response.getDataLocacao()), not(equalTo(null)));
-		error.checkThat("Falso",formatDateToDDMMYYY(response.getDataRetorno()) , not(equalTo(null)));
-		error.checkThat(response.getUsuario().getNome() , not(equalTo(null)));
-		
+		error.checkThat("Falso", formatDateToDDMMYYY(response.getDataRetorno()), not(equalTo(null)));
+		error.checkThat(response.getUsuario().getNome(), not(equalTo(null)));
+
 		/*
-		 	error.checkThat("Falso",formatDateToDDMMYYY(response.getDataRetorno()) , not(equalTo(formatDateToDDMMYYY(response.getDataRetorno()))));
-			error.checkThat(response.getUsuario().getNome() , not(equalTo("Bruno")));
+		 * error.checkThat("Falso",formatDateToDDMMYYY(response.getDataRetorno()) ,
+		 * not(equalTo(formatDateToDDMMYYY(response.getDataRetorno()))));
+		 * error.checkThat(response.getUsuario().getNome() , not(equalTo("Bruno")));
 		 */
+
+	}
+
+	// 3 unidades desconto de 25%
+	@Test
+	public void descontoAluguelFilmeTresUnidades() throws Exception {
+		Filme filme = new Filme("A fulga das galinhas", 1, 5.7);
+		Filme filme2 = new Filme("A fulga das galinhas", 1, 5.7);
+		Filme filme3 = new Filme("A fulga das galinhas", 1, 5.7);
+		Double valor =  filme3.getPrecoLocacao() - (filme3.getPrecoLocacao() * 25 / 100);
+		Locacao locacao = mockLocacaoFilmeComDesconto(Arrays.asList(filme, filme2, filme3));		
+		valor = valor + filme.getPrecoLocacao() + filme2.getPrecoLocacao();
+		assertThat(locacao.getValor(), is(equalTo(valor)));
+
+	}
+	
+	@Test
+	public void descontoAluguelFilmeQuatroUnidades() throws Exception {
+		Filme filme = new Filme("A fulga das galinhas", 1, 5.0);
+		Filme filme2 = new Filme("A fulga das galinhas", 1, 5.0);
+		Filme filme3 = new Filme("A fulga das galinhas", 1, 5.0);
+		Filme filme4 = new Filme("A fulga das galinhas", 1, 5.0);
+		Double valor3 =  filme3.getPrecoLocacao() - (filme3.getPrecoLocacao() * 25 / 100);
+		Double valor =  filme4.getPrecoLocacao() - (filme4.getPrecoLocacao() * 50 / 100);
 		
+		Locacao locacao = mockLocacaoFilmeComDesconto(Arrays.asList(filme, filme2, filme3, filme4));		
+		valor = valor + filme.getPrecoLocacao() + filme2.getPrecoLocacao() + valor3;
+		assertThat(locacao.getValor(), is(equalTo(valor)));
+
 	}
 	
+	@Test
+	public void descontoAluguelFilmeCincoUnidades() throws Exception {
+		Filme filme = new Filme("A fulga das galinhas", 1, 5.0);
+		Filme filme2 = new Filme("A fulga das galinhas", 1, 5.0);
+		Filme filme3 = new Filme("A fulga das galinhas", 1, 5.0);
+		Filme filme4 = new Filme("A fulga das galinhas", 1, 5.0);
+		Filme filme5 = new Filme("A fulga das galinhas", 1, 5.0);
+		Double valor3 =  filme3.getPrecoLocacao() - (filme3.getPrecoLocacao() * 25 / 100);
+		Double valor4 =  filme4.getPrecoLocacao() - (filme4.getPrecoLocacao() * 50 / 100);
+		Double valor =  filme5.getPrecoLocacao() - (filme5.getPrecoLocacao() * 75 / 100);
+		Locacao locacao = mockLocacaoFilmeComDesconto(Arrays.asList(filme, filme2, filme3, filme4, filme5));		
+		valor = valor + filme.getPrecoLocacao() + filme2.getPrecoLocacao() + valor3 + valor4;
+		assertThat(locacao.getValor(), is(equalTo(valor)));
+
+	}
 	
-	
+	@Test
+	public void descontoAluguelFilmeSeisUnidades() throws Exception {
+		Filme filme = new Filme("A fulga das galinhas", 1, 5.0);
+		Filme filme2 = new Filme("A fulga das galinhas", 1, 5.0);
+		Filme filme3 = new Filme("A fulga das galinhas", 1, 5.0);
+		Filme filme4 = new Filme("A fulga das galinhas", 1, 5.0);
+		Filme filme5 = new Filme("A fulga das galinhas", 1, 5.0);
+		Filme filme6 = new Filme("A fulga das galinhas", 1, 5.0);
+		
+		Double valor3 =  filme3.getPrecoLocacao() - (filme3.getPrecoLocacao() * 25 / 100);
+		Double valor4 =  filme4.getPrecoLocacao() - (filme4.getPrecoLocacao() * 50 / 100);
+		Double valor5 =  filme5.getPrecoLocacao() - (filme5.getPrecoLocacao() * 75 / 100);
+		
+		Double valor =  filme6.getPrecoLocacao() - (filme6.getPrecoLocacao() * 100 / 100);
+		Locacao locacao = mockLocacaoFilmeComDesconto(Arrays.asList(filme, filme2, filme3, filme4, filme5, filme6));		
+		valor = valor + filme.getPrecoLocacao() + filme2.getPrecoLocacao() + valor3 + valor4 + valor5;
+		assertThat(locacao.getValor(), is(equalTo(valor)));
+
+	}
+
 	@Test(expected = LocadoraException.class)
-	public void UsuarioNulo() throws Exception{
-		mockLocacaoUsuarioNulo();		
+	public void UsuarioNulo() throws Exception {
+		mockLocacaoUsuarioNulo();
 	}
-	
+
 	@Test(expected = LocadoraException.class)
-	public void locacaoFilmeNulo() throws Exception{
-		mockLocacaoFilmeNulo();		
+	public void locacaoFilmeNulo() throws Exception {
+		mockLocacaoFilmeNulo();
 	}
-	
+
 	@Test(expected = RuntimeException.class)
-	public void locacaoSemFilme() throws Exception{
-		mockLocacaoFilmeSemEstoque(0);		
+	public void locacaoSemFilme() throws Exception {
+		mockLocacaoFilmeSemEstoque(0);
 	}
-	
-	
+
 	@Test
 	public void locacaoSemFilmeV2() throws Exception {
 		try {
 			mockLocacaoFilmeSemEstoque(0);
 			fail("Falhou por não lançar exceção");
 		} catch (RuntimeException e) {
-			assertThat(e.getMessage(), is(equalTo("br.com.bbnsdevelop.exceptions.FilmeSemEstoqueException: filme sem estoque")));
+			assertThat(e.getMessage(),
+					is(equalTo("br.com.bbnsdevelop.exceptions.FilmeSemEstoqueException: filme sem estoque")));
 		}
-		
+
 	}
-	
+
 	@Test
-	public void testUsuarioNuloV3() throws Exception{
+	public void testUsuarioNuloV3() throws Exception {
 		// tem que ser verificada antes da execução do cenário, para não falhar
 		ex.expect(LocadoraException.class);
 		ex.expectMessage("Usuário não pode ser vazio");
 		mockLocacaoUsuarioNulo();
-		
+
 	}
-	
+
 	@Test
-	public void testFilmeNuloV3() throws Exception{
+	public void testFilmeNuloV3() throws Exception {
 		// tem que ser verificada antes da execução do cenário, para não falhar
 		ex.expect(LocadoraException.class);
 		ex.expectMessage("Filme não pode ser vazio");
 		mockLocacaoFilmeNulo();
-		
+
 	}
-	
+
 	@Test
-	public void locacaoSemFilmeV3() throws Exception{
+	public void locacaoSemFilmeV3() throws Exception {
 		// tem que ser verificada antes da execuao do cenário, para não falhar
 		ex.expect(RuntimeException.class);
 		ex.expectMessage("filme sem estoque");
 
 		mockLocacaoFilmeSemEstoque(0);
-		
+
 	}
-	
+
 	@Test
-	public void testeObterValor() throws Exception{
+	public void testeObterValor() throws Exception {
 		Locacao response = mockLocacao();
 		assertTrue(response.getValor() != 0);
 	}
-	
+
 	@Test
-	public void testeObterDataLocacao() throws Exception{
+	public void testeObterDataLocacao() throws Exception {
 		Locacao response = mockLocacao();
 		assertTrue(formatDateToDDMMYYY(response.getDataLocacao()) != null);
 	}
-	
-	
+
 	@Test
-	public void testeObterDataRetorno() throws Exception{
+	public void testeObterDataRetorno() throws Exception {
 		Locacao response = mockLocacao();
-		assertTrue("Falso",formatDateToDDMMYYY(response.getDataRetorno()) != null);
+		assertTrue("Falso", formatDateToDDMMYYY(response.getDataRetorno()) != null);
 	}
-	
+
 	@Test
-	public void testeObterUsuario() throws Exception{
+	public void testeObterUsuario() throws Exception {
 		Locacao response = mockLocacao();
 		assertTrue(response.getUsuario().getNome() != null);
 	}
-	
-	private Locacao mockLocacao() throws Exception{
-		
-		Usuario usuario = new Usuario("Bruno"); 
+
+	private Locacao mockLocacao() throws Exception {
+
+		Usuario usuario = new Usuario("Bruno");
 		Filme filme = new Filme("A fulga das galinhas", 1, 5.7);
 		Filme filme2 = new Filme("A fulga das galinhas", 1, 5.7);
 		List<Filme> filmes = Arrays.asList(filme, filme2);
-		
-		return locacaoService.alugarFilme(usuario,filmes);
-		
+
+		return locacaoService.alugarFilme(usuario, filmes);
+
 	}
-	
-	private Locacao mockLocacaoFilmeSemEstoque(Integer estoque) throws Exception{
-		
-		Usuario usuario = new Usuario("Bruno"); 
+
+	private Locacao mockLocacaoFilmeComDesconto(List<Filme> filmes) throws Exception {
+		Usuario usuario = new Usuario("Bruno");		
+		return locacaoService.alugarFilme(usuario, filmes);
+
+	}
+
+	private Locacao mockLocacaoFilmeSemEstoque(Integer estoque) throws Exception {
+
+		Usuario usuario = new Usuario("Bruno");
 		Filme filme = new Filme("A fulga das galinhas", estoque, 5.7);
 		List<Filme> filmes = Arrays.asList(filme);
-		
-		return locacaoService.alugarFilme(usuario,filmes);
-		
+
+		return locacaoService.alugarFilme(usuario, filmes);
+
 	}
-	
-	private Locacao mockLocacaoUsuarioNulo() throws Exception{
-		
-		Usuario usuario = null; 
+
+	private Locacao mockLocacaoUsuarioNulo() throws Exception {
+
+		Usuario usuario = null;
 		Filme filme = new Filme("A fulga das galinhas", 1, 5.7);
 		List<Filme> filmes = Arrays.asList(filme);
-		
-		return locacaoService.alugarFilme(usuario,filmes);
-		
+
+		return locacaoService.alugarFilme(usuario, filmes);
+
 	}
-	
-	private Locacao mockLocacaoFilmeNulo() throws Exception{
-		Usuario usuario = new Usuario("Bruno"); 
-		List<Filme> filmes =new ArrayList<Filme>();
-		
-		return locacaoService.alugarFilme(usuario,filmes);
-		
+
+	private Locacao mockLocacaoFilmeNulo() throws Exception {
+		Usuario usuario = new Usuario("Bruno");
+		List<Filme> filmes = new ArrayList<Filme>();
+
+		return locacaoService.alugarFilme(usuario, filmes);
+
 	}
 }
